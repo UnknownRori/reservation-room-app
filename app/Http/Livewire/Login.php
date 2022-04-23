3052eb60ee2612f86ade0d07cf925730f07c8f6e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-04-23 14:52:21 +0700

    implement livewire authentication

--- app/Http/Livewire/Login.php
+++ app/Http/Livewire/Login.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Login extends Component
 {
+    public $name, $password;
+
+    protected $rules = [
+        'name' => ['string', 'min:4'],
+        'password' => ['string', 'min:4'],
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function update()
+    {
+        $credentials = $this->validate();
+        if (Auth::attempt($credentials)) return redirect()->route('home')->with('msg', ['success', 'Login success!']);
+        session()->flash('message', 'Wrong Username or Password!');
+    }
+
     public function render()
     {
         return view('livewire.login');
