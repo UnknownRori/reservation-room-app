ae300bd31deafe9707b32b733ecbcbb4b5e4e4e5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-04-23 12:02:05 +0700

    adding another livewire component

---
+++ app/Http/Livewire/HotelFacilityForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HotelFacilityForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.hotel-facility-form');
+    }
+}
---
+++ app/Http/Livewire/RoomFacilityForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RoomFacilityForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.room-facility-form');
+    }
+}
