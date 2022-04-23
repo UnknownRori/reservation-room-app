ccf82a323d27c418ce48cbaa181bcefe4d8ce0ed
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-04-23 12:12:28 +0700

    register route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\HotelFacilityController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\RoomFacilityController;
+use App\Http\Controllers\UserController;
+use App\Http\Livewire\Login;
+use App\Http\Livewire\Register;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +21,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+
+Route::middleware('auth.no')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::get('/login', Login::class)->name('login');
+        Route::get('/register', Register::class)->name('register');
+    });
+});
+
+Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index');
+Route::get('/rooms/{room:no_room}', [RoomController::class, 'show'])->name('rooms.show');
+// Route::get('/rooms/{room:no_room}/{roomfacility:name}', [RoomFacilityController::class, 'show'])->name('rooms.facility.show');
+Route::get('/facilities', [HotelFacilityController::class, 'index'])->name('hotelfacility.index');
+// Route::get('/facilities/{hotelfacility:name}', [HotelFacilityController::class, 'show'])->name('hotelfacility.show');
+
+Route::middleware('auth')->group(function () {
+    Route::post('/auth/logout', [UserController::class, 'logout'])->name('logout');
+    Route::get('/user/{user}/{orders}', [OrdersController::class, 'show'])->name('user.order.show');
+});
+
+Route::middleware('auth.receptionist')->group(function () {
+    Route::prefix('receptionist')->group(function () {
+        Route::get('/rooms', [RoomController::class, 'index'])->name('receptionist.rooms.index');
+        Route::get('/orders', [OrdersController::class, 'index'])->name('receptionist.orders.index');
+    });
+});
+
+Route::middleware('auth.admin')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/rooms', [RoomController::class, 'index'])->name('admin.rooms.index');
+        Route::get('/rooms/create', [RoomController::class, 'create'])->name('admin.rooms.create');
+        Route::get('/rooms/{room:no_room}', [RoomFacilityController::class, 'create'])->name('admin.room.facility.create');
+        Route::get('/facilities', [HotelFacilityController::class, 'index'])->name('admin.facility.index');
+        Route::get('/facilities/create', [HotelFacilityController::class, 'create'])->name('admin.facility.create');
+    });
 });
