f5a92cb0bf88b5c53d27be243b4406b909a86dca
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-04-26 08:02:28 +0700

    divide the access between admin and receptionist

--- resources/views/components/dashboard-navbar.blade.php
+++ resources/views/components/dashboard-navbar.blade.php
@@ -10,57 +10,68 @@
                 <li class="nav-item">
                     <a href="{{ route('home') }}" class="nav-link {{ $title == 'Home' ? 'active' : '' }}">Home</a>
                 </li>
-                <li class="dropdown nav-item">
-                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                        Room
-                    </a>
-                    <div class="dropdown-menu">
-                        <a href="{{ route('admin.rooms.index') }}"
-                            class="dropdown-item {{ $title == 'Rooms List' ? 'active' : '' }}">Rooms List</a>
-                        <a href="{{ route('admin.rooms.create') }}"
-                            class="dropdown-item {{ $title == 'Create new room' ? 'active' : '' }}">Create
-                            Room</a>
-                    </div>
-                </li>
-                <li class="dropdown nav-item">
-                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                        Room Facility
-                    </a>
-                    <div class="dropdown-menu">
-                        <a href="{{ route('admin.room.facility.index') }}"
-                            class="dropdown-item {{ $title == 'Rooms Facility List' ? 'active' : '' }}">Rooms
-                            Facility List</a>
-                        <a href="{{ route('admin.room.facility.create') }}"
-                            class="dropdown-item {{ $title == 'Create new room facility' ? 'active' : '' }}">Create
-                            Room Facility</a>
-                    </div>
-                </li>
-                <li class="dropdown nav-item">
-                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                        Room Type
-                    </a>
-                    <div class="dropdown-menu">
-                        <a href="{{ route('admin.roomtype.index') }}"
-                            class="dropdown-item {{ $title == 'Room Type List' ? 'active' : '' }}">Room
-                            Type List</a>
-                        <a href="{{ route('admin.roomtype.create') }}"
-                            class="dropdown-item {{ $title == 'Create new room type' ? 'active' : '' }}">Create
-                            Room Type</a>
-                    </div>
-                </li>
-                <li class="dropdown nav-item">
-                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                        Hotel Facility
-                    </a>
-                    <div class="dropdown-menu">
-                        <a href="{{ route('admin.facility.index') }}"
-                            class="dropdown-item {{ $title == 'Hotel Facility List' ? 'active' : '' }}">Hotel
-                            Facility List</a>
-                        <a href="{{ route('admin.facility.create') }}"
-                            class="dropdown-item {{ $title == 'Create new hotel facility' ? 'active' : '' }}">Create
-                            Hotel Facility</a>
-                    </div>
-                </li>
+                @if (Auth::user()->roles == 'admin')
+                    <li class="dropdown nav-item">
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                            Room
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="{{ route('admin.rooms.index') }}"
+                                class="dropdown-item {{ $title == 'Rooms List' ? 'active' : '' }}">Rooms List</a>
+                            <a href="{{ route('admin.rooms.create') }}"
+                                class="dropdown-item {{ $title == 'Create new room' ? 'active' : '' }}">Create
+                                Room</a>
+                        </div>
+                    </li>
+                    <li class="dropdown nav-item">
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                            Room Facility
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="{{ route('admin.room.facility.index') }}"
+                                class="dropdown-item {{ $title == 'Rooms Facility List' ? 'active' : '' }}">Rooms
+                                Facility List</a>
+                            <a href="{{ route('admin.room.facility.create') }}"
+                                class="dropdown-item {{ $title == 'Create new room facility' ? 'active' : '' }}">Create
+                                Room Facility</a>
+                        </div>
+                    </li>
+                    <li class="dropdown nav-item">
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                            Room Type
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="{{ route('admin.roomtype.index') }}"
+                                class="dropdown-item {{ $title == 'Room Type List' ? 'active' : '' }}">Room
+                                Type List</a>
+                            <a href="{{ route('admin.roomtype.create') }}"
+                                class="dropdown-item {{ $title == 'Create new room type' ? 'active' : '' }}">Create
+                                Room Type</a>
+                        </div>
+                    </li>
+                    <li class="dropdown nav-item">
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                            Hotel Facility
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="{{ route('admin.facility.index') }}"
+                                class="dropdown-item {{ $title == 'Hotel Facility List' ? 'active' : '' }}">Hotel
+                                Facility List</a>
+                            <a href="{{ route('admin.facility.create') }}"
+                                class="dropdown-item {{ $title == 'Create new hotel facility' ? 'active' : '' }}">Create
+                                Hotel Facility</a>
+                        </div>
+                    </li>
+                @elseif(Auth::user()->roles == 'receptionist')
+                    <li class="nav-item">
+                        <a href="{{ route('receptionist.orders.index') }}"
+                            class="nav-link {{ $title == 'Orders List' ? 'active' : '' }}">Orders List</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('receptionist.rooms.index') }}"
+                            class="nav-link {{ $title == 'Rooms List' ? 'active' : '' }}">Room List</a>
+                    </li>
+                @endif
             </ul>
             <x-authentication-card>
                 <x-slot name="title">{{ $title }}</x-slot>
